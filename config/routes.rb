404a12197c69d75d36ee1503aefe04fe288452d0
Jeremy Wiebe <jeremyw76@live.com>
2019-11-03 21:26:53 -0600

    Added routes and controller methods for customer filters
    - Customers can either all be viewed with no sorting, sorted by full
    name, or be filtered to show only customers with no email address.
    - Created routes and controller methods to facilitate these filters.
    - Added controls to the customers show view to select the desired
    filter.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'customers#show'
+  get '/customers/alphabetized', to: 'customers#show_alphabetized', as: 'alphabetized_customers'
+  get '/customers/missing_email', to: 'customers#show_missing_email', as: 'missing_email_customers'
 end
